2754057c4c7408d2a4e9eba385d7af72a4337bc2
k8mcelhnaey <kqmcelhaney@gmail.com>
2024-04-04 20:32:11 -0600

    feeding part 3 completed

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
@@ -41,3 +41,17 @@ def turtles_detail(request, turtle_id):
     # include the cat and feeding_form in the context
     'turtle': turtle, 'feeding_form': feeding_form
   })
+
+def add_feeding(request, turtle_id):
+    # process the form requets from the client
+    form = FeedingForm(request.POST)
+    #request>POST is like req.body, its the contents of the form
+    #validate the form
+    if form.is_valid():
+        #create an in memory instance (on django) or our data
+        #to be added to psql, commit=False, dont save to db yet
+        new_feeding =form.save(commit=False)
+        # now we want to make sure we add the cat id to the new_feeding
+        new_feeding.turtle_id = turtle_id
+        new_feeding.save() # this is adding a feeding row to the the feeding table in psql
+    return redirect('detail', turtle_id=turtle_id) #turtle_id is the name of the param in the url path, turtle_id is the id o fthe turtle from the url request
